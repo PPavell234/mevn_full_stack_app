29a6b7544328f32d8ef6c3ba6f68361ceb08d0fc
PPavell234 <varvardidl@gmail.com>
2025-10-16 17:25:10 +1000

    Подключение к серверу

--- server/app.js
+++ server/app.js
@@ -11,14 +11,17 @@ const port = process.env.port // 5000
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(express.static("upload"));
+app.use(express.static("uploads"));
 
 
 //подключение бд
-
+// @ts-ignore
 mongoose.connect(process.env.DB_URL,{
     useNewUrlParser:true,
-    
+    useUnifiedTopology:true,
+    useFindAndModify:true,
+    useCreateIndex:true,
+}).then(()=> console.log("Connected to the database!"))
+.catch((err)=>console.log(err));
 
-})
-
+app.listen(port, ()=> console.log('server runnign at http://localhost:${port}'));
