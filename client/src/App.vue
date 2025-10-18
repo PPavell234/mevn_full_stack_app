1d10aaf2067e44faca9ab65209c9778e3adec535
PPavell234 <varvardidl@gmail.com>
2025-10-18 15:06:23 +1000

    Вкладка

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,49 @@
 <template>
   <v-app id="inspire">
-
-    <v-app-bar>
+    
+<v-app-bar>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-app-bar-title>Application</v-app-bar-title>
     </v-app-bar>
+    
     <v-navigation-drawer v-model="drawer">
-      <!--  -->
+
+      
+
+
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>
+            DCodeMania
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            MEVN Full Stack App
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list density="compact">
+
+      <v-list-item
+        v-for="(item, i) in items"
+        :key="i"
+        :value="item"
+        color="primary"
+      >
+        <template v-slot:prepend>
+          <v-icon :icon="item.icon"></v-icon>
+        </template>
+
+        <v-list-item-title v-text="item.text"></v-list-item-title>
+      </v-list-item>
+    </v-list>
+
+      
     </v-navigation-drawer>
+    
 
     
 
@@ -26,6 +61,9 @@
 
 <script>
   export default {
-    data: () => ({ drawer: null }),
+  data: () => ({
+    drawer: null,
+      
+     }),
   }
 </script>
