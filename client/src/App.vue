c18e539b0dbb6fa7f08a547ad86477d70db9a783
PPavell234 <varvardidl@gmail.com>
2025-10-18 22:52:43 +1000

    Добавление сервера

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,91 +1,26 @@
 <template>
-  <v-app id="inspire">
-    <!-- Верхняя панель без синего фона -->
-    <v-app-bar app>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
-    </v-app-bar>
-
-    <!-- Боковое меню -->
-    <v-navigation-drawer app v-model="drawer">
-      <!-- Заголовок -->
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title">DCodeMania</v-list-item-title>
-          <v-list-item-subtitle>MEVN Full Stack App</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <!-- Карточка со списком -->
-      <v-card flat>
-        <v-list dense>
-          <v-subheader>REPORTS</v-subheader>
-
-          <v-list-item
-            v-for="(item, i) in items"
-            :key="i"
-            :class="{ 'active-item': selected === item.text }"
-            @click="selectItem(item.text)"
-            :to="item.link"
-            link
-          >
-            <v-list-item-icon>
-              <v-icon :color="selected === item.text ? 'white' : 'primary'">
-                {{ item.icon }}
-              </v-icon>
-            </v-list-item-icon>
-
-            <v-list-item-content>
-              <v-list-item-title
-                :class="{ 'white--text': selected === item.text }"
-              >
-                {{ item.text }}
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-card>
-    </v-navigation-drawer>
-
-
-    <v-main>
-      <router-view></router-view>
-    </v-main>
-
-    
-  </v-app>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
   name: 'App',
-  data: () => ({
-    drawer: null,
-    selected: 'Real-Time',
-    items: [
-      { text: 'Home', icon: 'mdi-home',link:"/" },
-      { text: 'Add Post', icon: 'mdi-note-plus',link:"/add-post" },
-      { text: 'About', icon: 'mdi-help',link:"/about" },
-    ],
-  }),
-  methods: {
-    selectItem(item) {
-      this.selected = item
-    },
-  },
+  components: {
+    HelloWorld
+  }
 }
 </script>
 
 <style>
-.active-item {
-  background-color: #1976d2 !important; /* Синий фон активного пункта */
-  color: white !important;
-  transition: background-color 0.3s ease;
-}
-
-.v-list-item:hover:not(.active-item) {
-  background-color: rgba(25, 118, 210, 0.1) !important;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
