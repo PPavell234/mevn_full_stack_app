f9dc91877bf62b4dff38fd7b6ba2403bbaafdf40
PPavell234 <varvardidl@gmail.com>
2025-10-18 18:17:21 +1000

    Добавление карточек

---
+++ client/src/api.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const url = "http://localhost:5000/api/post";
+
+export default class API {
+  // Получить все посты
+  static async getAllPost() {
+    const res = await axios.get(url);
+    return res.data;
+  }
+
+  // Получить пост по ID
+  static async getPostByID(id) {
+    const res = await axios.get(`${url}/${id}`);
+    return res.data;
+  }
+
+  // Добавить новый пост
+  static async addPost(post) {
+    const res = await axios.post(url, post);
+    return res.data;
+  }
+
+  // Обновить пост по ID
+  static async updatePost(id, post) {
+    const res = await axios.patch(`${url}/${id}`, post);
+    return res.data;
+  }
+
+  // Удалить пост по ID
+  static async deletePostByID(id) {
+    const res = await axios.delete(`${url}/${id}`);
+    return res.data;
+  }
+}
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,51 @@
 <template>
-  <h1>Home Page</h1>
+  <div>
+    <h1>Home Page</h1>
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          sm="4"
+          class="pa-3"
+          v-for="post in posts"
+          :key="post._id"
+        >
+          <v-card class="pa-1">
+            <!-- исправлено :src -->
+            <v-img height="250" :src="`http://localhost:5000/${post.image}`"></v-img>
+
+            <v-btn class="ml-4 mt-3" small outlined color="indigo">
+              {{ post.category }}
+            </v-btn>
+
+            <v-card-title class="headline">{{ post.title }}</v-card-title>
+
+            <v-card-text class="py-0">
+              <p>{{ (post.content || '').slice(0, 100) + '...' }}</p>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
+import API from "../api";
+
 export default {
-  name: 'Home',
+  name: "Home",
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  async created() {
+    try {
+      this.posts = await API.getAllPost();
+    } catch (error) {
+      console.error("Ошибка при загрузке постов:", error);
+    }
+  },
 };
 </script>
